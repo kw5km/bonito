8b189cced91b2221f8b2c5fa8fd6b71bac12a4ef
kw5km <kw5km@virginia.edu>
2023-03-02 16:46:55 -0500

    edit distance for basecalls updated

--- bonito/my_utils/called_alignment_bonito.py
+++ bonito/my_utils/called_alignment_bonito.py
@@ -29,7 +29,7 @@ plt.rcParams.update({'font.size': 14})
 plt.rcParams['mathtext.fontset'] = 'stix'
 plt.rcParams['font.family'] = 'STIXGeneral'
 
-n, bp, ver = 1000, 200, '2'
+n, bp, ver = 1000, 200, '3-4'
 
 bon_mean = []
 bon_std = []
@@ -45,224 +45,75 @@ for delt, r in zip(deltas, rates):
 
     # 0 is entirely unconstrained so no state split
     if delt == '000':
-        path_var_ref = '/home/kw5km/work/mnt/kw5km/Research/Nanopore_Channel/Data_new/deBruijn_6mer_mean/n{}_bp{}_delta{}_DeepSimu/fasta_raw/'.format(n, bp, delt)
+        path_var_ref = '/home/kw5km/work/mnt/kw5km/Research/Nanopore_Channel/Data_new/deBruijn_6mer_mean/n{}_bp{}_delta{}_DeepSimu/fasta/'.format(n, bp, delt)
         basecall_file = 'n{}_bp{}_delta{}@v{}.fastq'.format(n, bp, delt, ver)
+        f_out = 'n{}_bp{}_delta{}@v{}/'.format(n, bp, delt, ver)
 
     else:
-        path_var_ref = 'home/kw5km/work/mnt/kw5km/Research/Nanopore_Channel/Data_new/deBruijn_6mer_mean/Encoded/n{}_bp{}_delta{}_statesplit_rate{}_DeepSimu/fasta_raw/'.format(n, bp, delt, r)
+        path_var_ref = '/home/kw5km/work/mnt/kw5km/Research/Nanopore_Channel/Data_new/deBruijn_6mer_mean/Encoded/n{}_bp{}_delta{}_statesplit_rate{}_DeepSimu/fasta/'.format(n, bp, delt, r)
         basecall_file = 'n{}_bp{}_delta{}_rate{}@v{}.fastq'.format(n, bp, delt, r, ver)
+        f_out = 'n{}_bp{}_delta{}_rate{}@v{}/'.format(n, bp, delt, r, ver)
 
     score_f, fig_f = '/scores/', '/Figs/'
     path_var_long = '/home/kw5km/work/mnt/kw5km/Research/Nanopore_Channel/Data_new/bonito/basecalls/'
-    path_var_out = path_var_long  
+    path_var_long_out = path_var_long+ f_out 
 
     bon_calls = {}
     filename_bon = path_var_long+basecall_file  
     print('Bonito file:', filename_bon)
     with open(filename_bon) as bon_file:
         for record in SeqIO.parse(bon_file, "fastq"):
-            bon_calls[record.id] = record.seq
-            print(record)
-            # print(record.id, record.seq)
+            r_desc = record.description
+            id_start, id_end = r_desc.find('signal'), r_desc.find(".fast5")
+            id = r_desc[id_start:id_end]
+            bon_calls[id] = record.seq
         bon_file.close() 
-    
-#     mapping = {}
-#     for filename in os.listdir(path_var_long+'/fast5/'):
-#         filename_list = filename.split('_')
-#         if 'signal' not in filename_list:continue
-#         mapping[filename_list[-2]] = filename_list[-1].replace(".fast5", "")
-
-#     cs = guppy_calls.values()
-
-#     scores_dwll, scores_ev , scores_guppy , scores_raw = [], [], [], []
-#     lev_dwll, lev_ev , lev_guppy , lev_raw, lev_vit = [], [], [], [], []
-#     errs_vit, errs_ev , errs_guppy , errs_raw = [], [], [], []
-
-#     FP_all_vit, FN_all_vit = np.array([]), np.array([])
-#     FP_all_ont, FN_all_ont = np.array([]), np.array([])
-
-
-#     for j in mapping.keys():
-
-#         # print(j)
-#         ''' Guppy '''
-#         mapping_id = mapping[j]
-
-#         if mapping_id in guppy_calls.keys(): 
-#             seq_guppy = guppy_calls[mapping_id]
-#         else: 
-#             seq_guppy = cs[0]  	
-#         j = int(j)
-
-#         ''' True '''
-#         filename_true = path_var_long+'processed_genome_{}'.format(j)  
-#         with open(filename_true) as true_file:
-#             for line in true_file:
-#                 if '>' not in line: bases_true = line
-#             true_file.close()
-#         seq_true = Seq(bases_true)
-
-#         ''' Viterbi Dwell '''
-#         filename_viterbi_dwll = path_var_long_out+calls_f+'j{}'.format(j)
-#         # if not Path(filename_viterbi_dwll).is_file(): continue
-#         if Path(filename_viterbi_dwll).is_file():
-#             with open(filename_viterbi_dwll) as viterbi_file_dwll:
-#                 for line in viterbi_file_dwll:
-#                     viterbi_kmers_dwll = line
-#                 viterbi_file_dwll.close()
-            
-#             ch = ['\'', '[', ']', '2', '3', ' ']
-#             # Remove multiple characters from the string
-#             for character in ch:
-#                 viterbi_kmers_dwll = viterbi_kmers_dwll.replace(character, '')  
-            
-#             viterbi_kmers_dwll = viterbi_kmers_dwll.split(',')
-                
-#             viterbi_seq_dwll = "" + viterbi_kmers_dwll[0]
-#             last_kmer = viterbi_kmers_dwll[0]
-#             viterbi_kmers_proc = [last_kmer]
-#             for b in viterbi_kmers_dwll[1:]:
-#                 if b == last_kmer: continue
-#                 else: 
-#                     viterbi_kmers_proc.append(b)
-#                     viterbi_seq_dwll=viterbi_seq_dwll+b[-1]
-#                     last_kmer=b
-#                 seq_dwll = Seq(viterbi_seq_dwll)
-
-#             file_out = path_var_long_out+calls_f+'j{}_processed'.format(j)
-#             with open(file_out, "w") as txt_file:
-#                     txt_file.write(str(viterbi_kmers_proc))
-#                     txt_file.close()
-
-#         ''' Events '''
-#         filename_ev = path_var_long + 'fasta/signal_{}_{}.fasta'.format(j, mapping_id)  
-#         with open(filename_ev) as ev_file:
-#             for line in ev_file:
-#                 if '>' not in line: bases_ev = line
-#             ev_file.close()
-#         seq_ev = Seq(bases_ev)
-
-#         ''' Raw '''       
-#         filename_raw = path_var_long + 'fasta_raw/signal_{}_{}.fasta'.format(j, mapping_id)  
-#         if not Path(filename_raw).is_file(): continue
-#         with open(filename_raw) as raw_file:
-#             for line in raw_file:
-#                 if '>' not in line: bases_raw = line
-#             raw_file.close()
-#         seq_raw = Seq(bases_raw)
-
-#         ''' Segs True'''
-#         # tru_seg = [0]
-#         # last = 0
-#         # alig = np.loadtxt(path_var_long+'align/align_{}.ali'.format(j))
-#         # for index in alig:
-#         #     if index[1] == last:
-#         #         continue
-#         #     tru_seg.append(index[0]-1)
-#         #     last = index[1]
-#         # df_b = np.array(tru_seg)
-#         # df_b = np.arange(bp)
-
-#         ''' Segs Event'''
-#         # filename_hdf5 = path_var_long+'hdf5/signal_{}_{}.hdf5'.format(j, mapping_id)
-#         # new_dat = read_dump_seq(filename_hdf5)
-            
-#         # ## ONT Segmentation
-#         # segs_ont=new_dat[:, 0]
-#         # db_ont, fn_ont, FN_ont, FP_ont = score_seg(df_b, segs_ont)
-
-#         ''' Segs Viterbi'''
-#         ## Get Viterbi estimated kmers
-#         # filename_figs_base = path_var_long_out+fig_f
-                
-#         # viterbi_kmers = viterbi_kmers_dwll
         
-#         ## Find the segments inferred from the Viterbi estimation kmers
-#         # segs_viterbi = [0]
-#         # for i, b in enumerate(viterbi_kmers[1:], 1):
-#         #     if b == viterbi_kmers[i-1]: 
-#         #         continue
-#         #     else: segs_viterbi.append(i)
-#         # db_viterbi, fn_viterbi, FN_viterbi, FP_viterbi = score_seg(df_b, segs_viterbi)
+    scores_bon = []
+    lev_bon = []
+    errs_bon = []
 
-#         # ''' Save Segs'''
-#         # # FP_all_ont = np.append(FP_all_ont, FP_ont)
-#         # # FN_all_ont = np.append(FN_all_ont, FN_ont)
-#         # FP_all_vit = np.append(FP_all_vit, FP_viterbi)
-#         # FN_all_vit = np.append(FN_all_vit, FN_viterbi)
+    for signal_id in bon_calls.keys():
+
+        print(signal_id)
+        ''' Bonito '''
+        seq_bon = bon_calls[signal_id]
+
+        ''' True '''
+        filename_true = path_var_ref+signal_id+'.fasta'
+        with open(filename_true) as true_file:
+            for line in true_file:
+                if '>' not in line: bases_true = line
+            true_file.close()
+        seq_true = Seq(bases_true)
+
         
-#         ''' Align '''
-#         if Path(filename_viterbi_dwll).is_file():
-#             vit_align = edlib.align(str(seq_dwll), str(seq_true), task = "path")
-#             vit_alignment = edlib.getNiceAlignment(vit_align, str(seq_dwll), str(seq_true))
-#             lev_vit.append(vit_align["editDistance"])
-#             vit_ins, vit_dels, vit_subs = format_cigar(vit_align["cigar"])
-#             errs_vit.append((vit_ins, vit_dels, vit_subs))
+        ''' Align '''
 
-#         guppy_align = edlib.align(str(seq_guppy), str(seq_true), task = "path")
-#         guppy_alignment = edlib.getNiceAlignment(guppy_align, str(seq_guppy), str(seq_true))
-#         lev_guppy.append(guppy_align["editDistance"])
-#         guppy_ins, guppy_dels, guppy_subs = format_cigar(guppy_align['cigar'])
-#         errs_guppy.append((guppy_ins, guppy_dels, guppy_subs))
+        bon_align = edlib.align(str(seq_bon), str(seq_true), task = "path")
+        bon_alignment = edlib.getNiceAlignment(bon_align, str(seq_bon), str(seq_true))
+        lev_bon.append(bon_align["editDistance"])
+        bon_ins, bon_dels, bon_subs = format_cigar(bon_align['cigar'])
+        errs_bon.append((bon_ins, bon_dels, bon_subs))
 
-#         ev_align = edlib.align(str(seq_ev), str(seq_true), task = "path")
-#         ev_alignment = edlib.getNiceAlignment(ev_align, str(seq_ev), str(seq_true))
-#         lev_ev.append(ev_align["editDistance"])
-#         ev_ins, ev_dels, ev_subs = format_cigar(ev_align["cigar"])
-#         errs_ev.append((ev_ins, ev_dels, ev_subs))
 
-#         raw_align = edlib.align(str(seq_raw), str(seq_true), task = "path")
-#         raw_alignment = edlib.getNiceAlignment(raw_align, str(seq_raw), str(seq_true))
-#         lev_raw.append(raw_align["editDistance"])
-#         raw_ins, raw_dels, raw_subs = format_cigar(raw_align["cigar"])
-#         errs_raw.append((raw_ins, raw_dels, raw_subs))
+        filepath_align = path_var_long_out+score_f
+        if not os.path.exists(filepath_align):
+            os.makedirs(filepath_align)
 
-#         filepath_align = path_var_long_out+score_f
-#         if not os.path.exists(filepath_align):
-#             os.makedirs(filepath_align)
-
-#         filename_align = filepath_align+'align_j{}.txt'.format(j)
-#         with open(filename_align, "w") as txt_file:
-#             txt_file.write('(top seq<-true) \n')
-#             if Path(filename_viterbi_dwll).is_file():
-#                 txt_file.write('Viterbi: \n')
-#                 txt_file.write("\n".join(vit_alignment.values())) 
-#                 txt_file.write('\n')
-#                 txt_file.write('Lev Dist: {}'.format(vit_align["editDistance"]))
-#                 txt_file.write('\n')
-#                 txt_file.write('Edits: {}'.format(format_cigar(vit_align["cigar"])))
-#                 txt_file.write('\n')
-#                 txt_file.write('\n')
-#             txt_file.write('Guppy: \n')
-#             txt_file.write("\n".join(guppy_alignment.values())) 
-#             txt_file.write('\n')
-#             txt_file.write('Lev Dist: {}'.format(guppy_align["editDistance"]))
-#             txt_file.write('\n')
-#             txt_file.write('Edits: {}'.format(guppy_align["cigar"]))
-#             txt_file.write('\n')
-#             txt_file.write('Insertions: {}, Deletions: {}, Substitutions: {}'.format(guppy_ins, guppy_dels, guppy_subs))
-#             txt_file.write('\n')
-#             txt_file.write('\n')
-#             txt_file.write('Raw: \n')
-#             txt_file.write("\n".join(raw_alignment.values()))  
-#             txt_file.write('\n')
-#             txt_file.write('Lev Dist: {}'.format(raw_align["editDistance"]))
-#             txt_file.write('\n')
-#             txt_file.write('Edits: {}'.format(raw_align["cigar"]))
-#             txt_file.write('\n')
-#             txt_file.write('Insertions: {}, Deletions: {}, Substitutions: {}'.format(raw_ins, raw_dels, raw_subs))
-#             txt_file.write('\n')
-#             txt_file.write('\n')
-#             txt_file.write('Event: \n')
-#             txt_file.write("\n".join(ev_alignment.values())) 
-#             txt_file.write('\n')
-#             txt_file.write('Lev Dist: {}'.format(ev_align["editDistance"])) 
-#             txt_file.write('\n')
-#             txt_file.write('Edits: {}'.format(ev_align["cigar"]))
-#             txt_file.write('\n')
-#             txt_file.write('\n')
-#             txt_file.write('Insertions: {}, Deletions: {}, Substitutions: {}'.format(ev_ins, ev_dels, ev_subs))
-#             txt_file.close()
+        filename_align = filepath_align+'align_{}.txt'.format(signal_id)
+        with open(filename_align, "w") as txt_file:
+            txt_file.write('(top seq<-true) \n')
+            txt_file.write('Bonito Version {}: \n'.format(ver))
+            txt_file.write("\n".join(bon_alignment.values())) 
+            txt_file.write('\n')
+            txt_file.write('Lev Dist: {}'.format(bon_align["editDistance"]))
+            txt_file.write('\n')
+            txt_file.write('Edits: {}'.format(bon_align["cigar"]))
+            txt_file.write('\n')
+            txt_file.write('Insertions: {}, Deletions: {}, Substitutions: {}'.format(bon_ins, bon_dels, bon_subs))
+            txt_file.write('\n')
+            txt_file.close()
 
 #         # sig = np.loadtxt(path_var_long+'signal/signal_{}.txt'.format(j))
 #         # sig = (sig-14)/5.7
